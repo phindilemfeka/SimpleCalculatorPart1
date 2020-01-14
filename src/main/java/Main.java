0382d94b8c815a8427cca9006989a3bb2ca272db
recruit <phindileportia20@gmail.com>
2020-01-14 14:36:45 +0200

    3rd commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 public class Main {
     public static void main(String[] args) {
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.ls.LSOutput;
 
 import static org.junit.jupiter.api.Assertions.*;
 
